1bbb35407815e55ddf40f81281614db311d413dc
veely1 <veely1@gmail.com>
2018-10-12 23:27:31 -0400

    fixed indentation

--- test/test_mars_rovers.js
+++ test/test_mars_rovers.js
@@ -4,16 +4,16 @@ const expect = chai.expect;
 const Rover = require('../mars_rovers.js');
 
 describe("navigate", function() {
-	let rover1, gridSize, instructions;
-	beforeEach(() => {
-		rover1 = new Rover("1 2 N");
-		instructions = "LMLMLMLMM";
-		rover1.navigate(instructions);
-	});
+  let rover1, gridSize, instructions;
+  beforeEach(() => {
+    rover1 = new Rover("1 2 N");
+    instructions = "LMLMLMLMM";
+    rover1.navigate(instructions);
+  });
 
-	context("When given instructions 'LMLMLMLMM'", () => {
-		it("should return '1 3 N'", function() {
-		  expect(rover1.roverPosition).to.equal("1 3 N");
-		});
-	});
+  context("When given instructions 'LMLMLMLMM'", () => {
+    it("should return '1 3 N'", function() {
+      expect(rover1.roverPosition).to.equal("1 3 N");
+    });
+  });
 });
