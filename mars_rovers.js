0c748f46beecd15dc8b91fcd2be5b2c5e5b7f1a7
veely1 <veely1@gmail.com>
2018-10-12 23:32:32 -0400

    rotateLeft and moveForward prototype

--- mars_rovers.js
+++ mars_rovers.js
@@ -9,6 +9,67 @@ class Rover {
   get roverPosition() {
     return [this.x, this.y, this.heading].join(' ');
   }
+
+  navigate(instructions) {
+    let splitInstructions = instructions.split('');
+    splitInstructions.map( instruction => {
+      switch(instruction) {
+        case 'L':
+          this.rotateLeft();
+          break;
+        case 'R':
+          this.rotateRight();
+          break;
+        case 'M':
+          this.moveForward();
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  rotateLeft() {
+    switch(this.heading) {
+      case 'N':
+        this.heading = 'W';
+        break;
+      case 'E':
+        this.heading = 'N'
+        break;
+      case 'S':
+        this.heading = 'E'
+        break;
+      case 'W':
+        this.heading = 'S'
+        break;
+      default:
+        break;
+    }
+  }
+
+  rotateRight() {
+
+  }
+
+  moveForward() {
+    switch(this.heading) {
+      case 'N':
+        this.y += 1;
+        break;
+      case 'E':
+        this.x += 1;
+        break;
+      case 'S':
+        this.y -= 1;
+        break;
+      case 'W':
+        this.x -= 1;
+        break;
+      default:
+        break;
+    }
+  }
 }
 
 module.exports = Rover;
