e657040446fac2cf3f0d8bed75d126335af5a989
veely1 <veely1@gmail.com>
2018-10-12 23:56:43 -0400

    fixed test description, changed getter 'roverPosition' to 'position'

--- test/test_mars_rovers.js
+++ test/test_mars_rovers.js
@@ -16,13 +16,13 @@ describe("navigate", function() {
 
   context("When rover at '1 2 N', given instructions 'LMLMLMLMM'", () => {
     it("should return '1 3 N'", function() {
-      expect(rover1.roverPosition).to.equal("1 3 N");
+      expect(rover1.position).to.equal("1 3 N");
     });
   });
 
   context("When rover at '3 3 E', given instructions 'MMRMMRMRRM'", () => {
-    it("should return '1 3 N'", function() {
-      expect(rover2.roverPosition).to.equal("5 1 E");
+    it("should return '5 1 E'", function() {
+      expect(rover2.position).to.equal("5 1 E");
     });
   });
 });
