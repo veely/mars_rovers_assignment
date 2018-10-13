4ef807a222fa8715657caef92d30de76ec08b882
veely1 <veely1@gmail.com>
2018-10-12 23:40:40 -0400

    added test case for rover navigation

--- test/test_mars_rovers.js
+++ test/test_mars_rovers.js
@@ -7,13 +7,22 @@ describe("navigate", function() {
   let rover1, gridSize, instructions;
   beforeEach(() => {
     rover1 = new Rover("1 2 N");
-    instructions = "LMLMLMLMM";
-    rover1.navigate(instructions);
+    rover2 = new Rover("3 3 E");
+    instructions1 = "LMLMLMLMM";
+    instructions2 = "MMRMMRMRRM";
+    rover1.navigate(instructions1);
+    rover2.navigate(instructions2);
   });
 
-  context("When given instructions 'LMLMLMLMM'", () => {
+  context("When rover at '1 2 N', given instructions 'LMLMLMLMM'", () => {
     it("should return '1 3 N'", function() {
       expect(rover1.roverPosition).to.equal("1 3 N");
+    });
+  });
+
+  context("When rover at '3 3 E', given instructions 'MMRMMRMRRM'", () => {
+    it("should return '1 3 N'", function() {
+      expect(rover2.roverPosition).to.equal("5 1 E");
     });
   });
 });
